0390ae34f055cd58458bb7edd5403d70577f17cf
Antony Jones <ant@interrobang.(none)>
2011-10-25 17:07:05 +0100

    Communication work

---
+++ grails-app/services/uk/co/desirableobjects/oauth/scribe/OACommunicationService.groovy
@@ -0,0 +1,22 @@
+package uk.co.desirableobjects.oauth.scribe
+
+import org.scribe.model.Token
+import org.scribe.model.Verb
+import org.scribe.model.OAuthRequest
+import org.scribe.oauth.OAuthService
+import org.scribe.model.Response
+
+class OACommunicationService {
+
+    static transactional = true
+
+    Response accessResource(OAuthService service, Token accessToken, Verb verb, String url) {
+
+        println 'requested'
+
+        OAuthRequest oAuthRequest = new OAuthRequest(verb, url)
+        service.signRequest(accessToken, oAuthRequest)
+        return oAuthRequest.send()
+
+    }
+}
