9086eb77b9295a52009d26a5b5f9110bc831da9b
Enrico Comiti <enrico.comiti@gmail.com>
2014-05-20 21:46:48 +0200

    Upgrade to Scribe 1.3.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
 
-        runtime 'org.scribe:scribe:1.3.6'
+        runtime 'org.scribe:scribe:1.3.7'
 
         test    'org.gmock:gmock:0.8.2',
                 'org.objenesis:objenesis:1.2'
