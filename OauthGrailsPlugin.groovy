6d19e993a5508fa21a176c9c360f0a9551903fe1
Antony Jones <aj@debtonator.io>
2014-12-07 17:29:43 +0000

    Fix grails oauth plugin metadata

--- OauthGrailsPlugin.groovy
+++ OauthGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 class OauthGrailsPlugin {
 
-    def version = "2.6"
+    def version = "2.6.1"
 
     def grailsVersion = "1.3.1 > *"
 
     def author = "Antony Jones, Peter Ledbrook"
-    def authorEmail = "aj#desirableobjects.co.uk"
+    def authorEmail = "aj@desirableobjects.co.uk"
     def title = "Oauth Plugin"
     def description = 'Provides oAuth integration for Grails, using the Scribe framework'
 
