0aa0395eae4499a090a5e2d1a4049b002eeac89d
Antony Jones <ant@enzy.org>
2015-07-03 17:58:36 +0100

    Update OauthGrailsPlugin.groovy

--- OauthGrailsPlugin.groovy
+++ OauthGrailsPlugin.groovy
@@ -9,6 +9,6 @@ class OauthGrailsPlugin {
     def title = "Oauth Plugin"
     def description = 'Provides oAuth integration for Grails, using the Scribe framework'
 
-    def documentation = "http://aiten.github.com/grails-oauth-scribe/"
-    def scm = [url: "https://github.com/aiten/grails-oauth-scribe"]
+    def documentation = "http://antony.github.com/grails-oauth-scribe/"
+    def scm = [url: "https://github.com/antony/grails-oauth-scribe"]
 }
